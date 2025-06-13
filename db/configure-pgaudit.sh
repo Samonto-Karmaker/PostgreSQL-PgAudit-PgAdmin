fc8743f8e1f5de32550b78cf43b9e96ddae50c3a
Samonto Karmaker <samonto0726@gmail.com>
2025-06-13 15:54:49 +0600

    refactor: update configure-pgaudit.sh to copy postgresql.conf and restart PostgreSQL before creating pgaudit extension

--- db/configure-pgaudit.sh
+++ db/configure-pgaudit.sh
@@ -1,8 +1,15 @@
+bash
+// filepath: db/configure-pgaudit.sh
 #!/bin/bash
 set -e
 
-echo "Creating pgaudit extension and enabling configuration..."
+echo "Copying custom postgresql.conf..."
+cp /etc/postgresql/postgresql.conf "$PGDATA/postgresql.conf"
 
+echo "Restarting PostgreSQL to load new config..."
+pg_ctl -D "$PGDATA" -m fast -w restart
+
+echo "Creating pgaudit extension and enabling configuration..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS pgaudit;
     ALTER SYSTEM SET pgaudit.log = 'all';
